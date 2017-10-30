c48be55b0cb6245527955fe6722afbba7fc1e399
Balogh Laszlo <balazs.ujvari@science.unideb.hu>
2017-10-30 12:48:49 +0100

    add instances to Plotter constructor

--- src/Plotter.cc
+++ src/Plotter.cc
@@ -2,14 +2,16 @@
 
 Plotter::Plotter()
 {
-  ;
+   fOut = new TFile("result.root", "RECREATE");
+   canv = new TCanvas("canc","display",800,400);
+   BM1DTree = new TTree("BM1DTree","BM1DTree");
+   BM1DTree->Branch("tl",&tl, "tl/D");
+   BM1DTree->Branch("xl",&xl, "xl/D");
+   
 }
 void Plotter::Plot(Int_t n, std::vector<Double_t> t, std::vector<Double_t> x){
-  fOut = new TFile("result.root", "RECREATE");
   g1 = new TGraph(n,&t[0],&x[0]);
-  canv = new TCanvas("canc","display",800,400);
-  TTree *BM1DTree = new TTree("BM1DTree","BM1DTree");
-  
+  canv->cd();
   g1->Draw();
   g1->SetLineColor(1);
   g1->SetLineWidth(1);
@@ -19,9 +21,8 @@ void Plotter::Plot(Int_t n, std::vector<Double_t> t, std::vector<Double_t> x){
   g1->GetYaxis()->SetTitle("X");
   g1->GetXaxis()->SetTitle("Time");
   
-  BM1DTree->Branch("tl",&tl, "tl/D");
-  BM1DTree->Branch("xl",&xl, "xl/D");
-  for (unsigned int i = 0; i < t.size(); i++){
+  // for (unsigned int i = 0; i < t.size(); i++){
+  for (unsigned int i = 0; i < n; i++){
     tl = t[i];
     xl = x[i];
     BM1DTree->Fill();
