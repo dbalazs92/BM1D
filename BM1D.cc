fb42b93a5fc9f74740dd8b87ec5d1549950d8c78
dbalazs92 <balazsdemeter92@gmail.com>
2017-10-31 12:07:39 +0100

    Version 1.5.3 - Another fix for Main file to call different runs

--- BM1D.cc
+++ BM1D.cc
@@ -7,22 +7,22 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    Int_t nSteps, nRuns, x0,x1,x2,x3;
+    Int_t nSteps, nRuns, p0,p1,x1,x2;
     Double_t mu1, mu2, sigma1, sigma2;
     string fileName="input.root";
     char random_type='u';
     Int_t vis, typeOfRun;
 
-    nSteps=nRuns=x0=x1=x2=x3=vis=typeOfRun=0;
+    nSteps=nRuns=p0=p1=x1=x2=vis=typeOfRun=0;
     mu1=mu2=sigma1=sigma2=0.0;
     if(argc==15)
     {
         nSteps=atoi(argv[1]);
         nRuns=atoi(argv[2]);
-        x0=atoi(argv[3]);
-        x1=atoi(argv[4]);
-        x2=atoi(argv[5]);
-        x3=atoi(argv[6]);
+        p0=atoi(argv[3]);
+        p1=atoi(argv[4]);
+        x1=atoi(argv[5]);
+        x2=atoi(argv[6]);
         mu1=atof(argv[7]);
         mu2=atof(argv[8]);
         sigma1=atof(argv[9]);
@@ -44,13 +44,13 @@ int main(int argc, char* argv[])
   myBM1DProcess->Init();
     switch(random_type){
         case 'u':
-            myBM1DProcess->Run(nRuns, nSteps, x0, x1);
+            myBM1DProcess->Run(nRuns, nSteps, p0, p1);
             break;
         case 'g':
-            myBM1DProcess->Run(nRuns, nSteps, x0, mu1, sigma1);
+            myBM1DProcess->Run(nRuns, nSteps, p0, mu1, sigma1);
             break;
         case 'l':
-            myBM1DProcess->Run(nRuns, nSteps, x0, x1, x2, mu1, sigma1, mu2, sigma2);
+            myBM1DProcess->Run(nRuns, nSteps, p0, x1, x2, mu1, sigma1, mu2, sigma2);
             break;
         default:
             cout<<"ERROR! Wrong parameter for type of random generator! \n No run!"<<endl;
